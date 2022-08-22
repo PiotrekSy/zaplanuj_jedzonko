f9e992b5a68ab429a098076cb06459108ccc239a
PiotrekSy <piotrsyjud@gmail.com>
2022-08-23 01:31:54 +0200

    202208230131

--- src/componentsHomePage/Carousel.js
+++ src/componentsHomePage/Carousel.js
@@ -1,21 +1,31 @@
-import React from "react";
-import reasons from "./Reasons";
+import React, {useState, useEffect} from "react";
+import db from "../firebase"
+import {collection, onSnapshot} from "firebase/firestore";
 
 const Carousel = () => {
-    return (
-        <div>
-            <div className="carouselLeftButton">aa</div>
-            <div className="carouselRightButton">aa</div>
-            <div className="carousel">
-                {reasons.map((item, index) =>
-                    <div className="carouselItem" key={index}>
-                        <div className="carouselTitle">{item.title}</div>
-                        <div className="carouselText">{item.text}</div>
-                    </div>
-                )}
-            </div>
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        onSnapshot(collection(db, "carousel"), (snapshot) => {
+            setData(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+
+    return (<div>
+        <div className="carouselLeftButton">aa</div>
+        <div className="carouselRightButton">aa</div>
+        <div className="carousel">
+            {/*TODO PAGINACJA PONIŻEJ => tło z firabase*/}
+            {data.map((item, index) =>
+                <div className="carouselItem"
+                     style={{backgroundColor: "yellow"}}
+                     key={index}>
+                    <div className="carouselTitle">{item.title}</div>
+                    <div className="carouselText">{item.text}</div>
+                </div>)}
         </div>
-    )
+    </div>)
 }
 
 export default Carousel;
