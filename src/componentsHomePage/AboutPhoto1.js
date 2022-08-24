110616a9105d0be0e6b0ed7997d7ad96c3ba94ce
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-08-24 19:44:08 +0200

    202208241944

--- src/componentsHomePage/AboutPhoto1.js
+++ src/componentsHomePage/AboutPhoto1.js
@@ -3,7 +3,7 @@ import React from "react";
 const AboutPhoto1 = () => {
     return (<>
             <div className="aboutPhoto photo1"/>
-            <div className="aboutName">Tory Stark</div>
+            <div className="aboutName">Tony Stark</div>
             <div className="aboutText">
                 <p>dietetyk, kucharz, posiadacz fajnej zbroi, filantrop, playboy, miliarder</p>
             </div>
