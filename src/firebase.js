f463ced67c79590aceeeff8f806bc58240101424
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-08-24 19:53:49 +0200

    202208241953

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDi2Ec64R2krZ5hHJc8daRZvpLRJaEshgs",
+    authDomain: "zaplanuj-jedzonko-7b08f.firebaseapp.com",
+    projectId: "zaplanuj-jedzonko-7b08f",
+    storageBucket: "zaplanuj-jedzonko-7b08f.appspot.com",
+    messagingSenderId: "169300596016",
+    appId: "1:169300596016:web:0136e793f19f73c575e4f5"
 };
 
 const app = initializeApp(firebaseConfig)
