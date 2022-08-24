481a930ecc28686c3d5cfddd0791741cc3636ac3
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-08-24 16:22:43 +0200

    202208241622

--- src/componentsHomePage/PhoneContact.js
+++ src/componentsHomePage/PhoneContact.js
@@ -14,32 +14,44 @@ const PhoneContact = () => {
         e.preventDefault();
         if (phone !== "" && hour !== "") {
             await addDoc(phoneRef, {phone, callMeAfter: hour});
+            console.log(`wysłano numer ${phone} z prośbą o kontakt po godzinie ${hour}`)
+            setPhone("")
+            setHour("")
+        } else {
+            console.log("nie wysyłam!")
         }
-        setPhone("")
-        setHour("")
     }
 
-    return (<div className="benefitsContactWrapper">
-        <div className="benefitsContactItem">
-            <div className="benefitsContactText">Chcesz dowiedzieć się więcej od naszego konsultanta? Zostaw swój numer
+    return (
+        <div className="benefitsContactWrapper">
+            <div className="benefitsContactText">Chcesz dowiedzieć się więcej od naszego konsultanta? Zostaw swój
+                numer
                 a na
                 pewno oddzwonimy!
             </div>
-            <form className="benefitsContactInputWrapper" onSubmit={sendNewsletterEmail}>
-                <label className="benefitsContactLabel">Proszę o kontakt na ten numer telefonu:
-                    <input placeholder="Twój 9-cyfrowy numer telefonu"
-                           value={phone}
-                           onChange={(e) => setPhone(e.target.value)}/>
-                </label>
-                <label className="benefitsContactLabel"> Najlepiej po godzinie:
-                    <input type="time"
-                           value={hour}
-                           onChange={(e) => setHour(e.target.value)}/>
-                    <button type="submit">Zadzwońcie do mnie!</button>
-                </label>
-            </form>
-        </div>
-    </div>)
+            <div className="benefitsContactItem">
+                <form className="benefitsContactInputWrapper" onSubmit={sendNewsletterEmail}>
+                    <div className="benefitsLabels">
+                        <div className="benefitsContactLabel">Proszę o kontakt na numer:
+                            <input className="benefitInput"
+                                   placeholder="Twój 9-cyfrowy numer telefonu"
+                                   value={phone}
+                                   onChange={(e) => setPhone(e.target.value)}/>
+                        </div>
+                        <div className="phoneValidation"></div>
+                        <div className="benefitsContactLabel"> Najlepiej po godzinie:
+                            <input className="benefitInput"
+                                   placeholder="Godzina po której życzysz sobie kontaktu"
+                                   value={hour}
+                                   onChange={(e) => setHour(e.target.value)}/>
+                        </div>
+                        <div className="hourValidation"></div>
+                    </div>
+                    <button type="submit" className="benefitsContactSubmitButton">Zadzwońcie do
+                        mnie{hour !== "" ? ` po ${hour}` : `!`}</button>
+                </form>
+            </div>
+        </div>)
 }
 
 export default PhoneContact;
