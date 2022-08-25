750acf716c32ad7e9f206abec2df72e1761dce99
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-08-25 02:22:14 +0200

    202208250222

--- src/componentsHomePage/Carousel.js
+++ src/componentsHomePage/Carousel.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, {useRef} from "react";
 import {reasons} from "./reasons";
 
 const Carousel = () => {
 
-    return (<div>
-        <div className="carouselLeftButton">aa</div>
-        <div className="carouselRightButton">aa</div>
-        <div className="carousel">
-            {reasons.map((item, index) =>
-                <div className="carouselItem"
-                     key={index}>
-                    <div className="carouselTitle">{item.title}</div>
-                    <div className="carouselText">{item.text}</div>
-                </div>)}
-        </div>
-    </div>)
+    let carousel = useRef()
+
+    const moveCarousel = delta => {
+
+        if (carousel.current) {
+            const width = carousel.current.offsetWidth;
+            carousel.current.scrollTo(carousel.current.scrollLeft + width * delta, 0)
+        }
+    }
+
+    return (
+        <div>
+            <div className="carousel">
+                <div className="carouselLeftButton" onClick={() => moveCarousel(-1)}>
+                    <div className="carouselIcon"></div>
+                </div>
+                <div className="carousel" ref={carousel}>
+                    {reasons.map((item, index) =>
+                        <div className="carouselItem"
+                             key={index}>
+                            <div className="carouselTitle">{item.title}</div>
+                            <div className="carouselText">{item.text}</div>
+                        </div>)}
+                </div>
+                <div className="carouselRightButton" onClick={() => moveCarousel(1)}>
+                    <div className="carouselIcon"></div>
+                </div>
+
+            </div>
+        </div>)
 }
 
 export default Carousel;
