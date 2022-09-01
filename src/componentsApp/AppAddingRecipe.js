5121e92352e77b45c9da1ed89964b4fec67aa0c5
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-09-01 14:09:45 +0200

    202209011409

--- src/componentsApp/AppAddingRecipe.js
+++ src/componentsApp/AppAddingRecipe.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import {useContext} from "react";
 import {AppPulpitContext} from "../context/AppPulpitContext";
+import {addDoc, collection} from "firebase/firestore";
+import db from "../firebase";
 
 const AppAddingRecipe = () => {
 
@@ -11,12 +13,70 @@ const AppAddingRecipe = () => {
         setView("main")
     }
 
+    const [ingredients, setIngredients] = useState([]);
+    const [newIngredient, setNewIngredient] = useState("");
+    //dodawanie składnika do przepisu:
+    const addIngredient = (e) => {
+        e.preventDefault();
+        setIngredients([...ingredients, newIngredient])
+        setNewIngredient("")
+    }
+    const [recipeName, setRecipeName] = useState("")
+    const [description, setDescription] = useState("")
+    const [error, setError] = useState("")
+    const phoneRef = collection(db, "recipes");
+
+    const sendRecipe = async (e) => {
+        e.preventDefault();
+        if (description !== "" && recipeName !== "" && ingredients.length > 1) {
+            await addDoc(phoneRef, {name: recipeName, description, ingredients});
+            console.log(`wysłano przepis:)`)
+            setRecipeName("")
+            setDescription("")
+            setIngredients([])
+            setError("Przepis wysłany :)")
+        } else {
+            setError("nie wysyłam, coś jest nie teges!")
+            console.log("nie wysyłam, coś jest nie teges!")
+        }
+    }
+
     return (
         <>
-            <div>
-                <div>DODAWANIE PRZEPISU</div>
-                <button onClick={backToMain}>Wróć do pulpitu</button>
-            </div>
+            <form className="addRecipeForm">
+                <div className="recipeName">
+                    <div>Nazwa przepisu:</div>
+                    <input type="text" value={recipeName} onChange={(e) => setRecipeName(e.target.value)}></input>
+                </div>
+
+                <div className="recipeIngredients">
+                    <div className="recipeIngredientsItems">
+                        <div>Dodaj składnik:</div>
+                        <div><input type="textarea" value={newIngredient}
+                                    onChange={(e) => setNewIngredient(e.target.value)}/>
+                            <button type="button" onClick={addIngredient}>+</button>
+                        </div>
+                    </div>
+                    <div className="ingredientsLabel">Twoje składniki:</div>
+                    <div className="recipeIngredientsList">
+
+                        <div>{ingredients.map((element) => element + ", ")}</div>
+                    </div>
+                </div>
+
+                <div className="recipeDescription">
+                    <div>Wpisz przepis:</div>
+                    <textarea value={description}
+                              onChange={(e) =>
+                                  setDescription(e.target.value)}>
+                        </textarea>
+                </div>
+                <div className="recipeError">{error}</div>
+                <div className="recipeButtons">
+                    <button type="button" onClick={backToMain}>WRÓĆ</button>
+                    <button type="submit" onClick={sendRecipe}>DODAJ PRZEPIS</button>
+                </div>
+            </form>
         </>
     )
 }
--- src/componentsApp/AppPulpitSavedPlans.js
+++ src/componentsApp/AppPulpitSavedPlans.js
@@ -2,6 +2,8 @@ import React from "react";
 
 const AppPulpitSavedPlans = () => {
 
+
+
     return (
         <div className="planDisplay">
 
